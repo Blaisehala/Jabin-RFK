3ac85d6bb223ed4d427289be822c896b5919a73e
Blaise hala <blaisehala@gmail.com>
2022-09-05 17:02:34 +0300

    BUG-deletes donate page views

--- jabin/views.py
+++ jabin/views.py
@@ -10,8 +10,7 @@ from jabin.models import Order
 def home(request):
     return render(request, 'users/home.html')
 
-def donate(request):
-    return render(request, 'users/donate.html')
+
 
 def about(request):
     return render(request, 'users/about.html')
